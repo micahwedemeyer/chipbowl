1af1a959e82a91c20054022159854d2f2f80a053
Micah Wedemeyer <me@micahwedemeyer.com>
2019-03-16 08:25:32 -0400

    Initial commit

---
+++ lib/chipbowl.ex
@@ -0,0 +1,18 @@
+defmodule Chipbowl do
+  @moduledoc """
+  Documentation for Chipbowl.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Chipbowl.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/chipbowl_test.exs
@@ -0,0 +1,8 @@
+defmodule ChipbowlTest do
+  use ExUnit.Case
+  doctest Chipbowl
+
+  test "greets the world" do
+    assert Chipbowl.hello() == :world
+  end
+end
